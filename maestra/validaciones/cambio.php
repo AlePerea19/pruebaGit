9413f3bf65f5de4064cc399924a73263ebfbad94
AlePerea19 <alex_92ch@hotmail.com>
2023-05-04 17:54:04 -0600

    Se agrego una plantillla de cambio

---
+++ maestra/validaciones/cambio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola soy un cambio de prueba para Git Hub</h1>
+    <h1>Arriba el Cruz Azul</h1>
+
+
+    <img src="../resources/img/rayoo.jpg" alt="El mas veloz de todos">
+</body>
+</html>
